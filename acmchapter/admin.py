74694d8774d64c175471d59bf5c79b7ad18ec05a
nagadeepsharma <nagadee.chandragiri@gmail.com>
2020-12-28 23:53:27 +0530

    vmegacm

--- acmchapter/admin.py
+++ acmchapter/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Upcomming,Past,Pastachievements,Projects
+from .models import Upcomming,Past,Pastachievements,Projects,Gallery
 
 # Register your models here.
 admin.site.register(Pastachievements)
 admin.site.register(Upcomming)
 admin.site.register(Past)
 admin.site.register(Projects)
+admin.site.register(Gallery)
